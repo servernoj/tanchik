13db755457d730d38ffda489962ed4c2a4f45e51
Simon Baev <simon.baev@globallogic.com>
2020-08-10 15:38:11 -0700

    add about

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .container
+    h1 Tanchik
+    NuxtLink(to='/') Home
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  h1 {
+    font-weight: bold
+  }
+</style>
